68560414d048bbcc3f7b04260a9a5919977578a9
heimansfeldmit1n <jannis@heimansfeld.de>
2025-05-27 16:31:34 +0200

    images by COMPANY

--- web/js/main.mjs
+++ web/js/main.mjs
@@ -6,7 +6,7 @@ const algorithms = {
   "fast": processFast,
   "slow": processSlow,
 };
-
+const company = document.getElementById('company').value;
 // from Xeact
 const u = (url = "", params = {}) => {
   let result = new URL(url, window.location.href);
@@ -15,7 +15,7 @@ const u = (url = "", params = {}) => {
 };
 
 const imageURL = (mood, cacheBuster, basePrefix) =>
-  u(`${basePrefix}/.within.website/x/cmd/anubis/static/img/${mood}.webp`, { cacheBuster });
+  u(`${basePrefix}/.within.website/x/cmd/anubis/static/img/${mood}_${company}.webp`, { cacheBuster });
 
 const dependencies = [
   {
